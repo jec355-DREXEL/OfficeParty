2047166d5939b02ee101cc4ecf7d16c53c7bccd7
glangenhorst <52493121+glangenhorst@users.noreply.github.com>
2019-10-06 14:52:42 -0400

    IDK honestly

---
+++ Prototype 2 God Folder/Office Party - Prototype 2/Assets/Scripts/EndScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
+{
+    public void MMLoad()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
